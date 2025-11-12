d6829274893c6b53644c9f0b5d5865197d257820
marcostranisci <marcoantonio.stranisci@unito.it>
2025-11-12 07:38:57 +0100

    added pages

--- src/create_pages.py
+++ src/create_pages.py
@@ -1,41 +1,58 @@
-post = 'Forza destra nazionalista patriottica di tutto il mondo fan c... la sinistra woke green lgtb.. pro invasione pro islam global-distruttiva'
-option1 = 'destra nazionalista'
-option2 = 'sì sì'
-option3 = 'bla bla'
-option4 = 'ciao'
-s = f'''
-<!DOCTYPE html>
-<html lang="en-us">
-  <head>
-    <meta charset="UTF-8">
-    <title>Cayman</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="theme-color" content="#157878">
-    <link rel="stylesheet" href="../css/normalize.css">
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="../css/cayman.css">
-  </head>
-  <body>
-    <section class="page-header">
-    </section>
+import pandas as pd
 
-    <section class="main-content">
-<h2>Read the Telegram Message</h2>
-<p>{post}</p>
+df = pd.read_csv('src/data.csv')
 
-<h2>Read the spans of text that highlight a conspiracy plan</h2>
-<ul>
-    <li><b>OPTION 1</b> {option1}</li>
-    <li><b>OPTION 2</b> {option2}</li>
-    <li><b>OPTION 3</b> {option3}</li>
-    <li><b>OPTION 4</b> {option4}</li>
-</ul>
+for _, row in df.iterrows():
+  post = row['text']
+  if row['all_plan']==0:
+    row.secret = eval(row['secret'])
+    option1 = row['secret'][0]
+    option2 = row['secret'][1]
+    option3 = row['secret'][2]
+    option4 = row['secret'][3]
+    inst = "Read the spans of text that highlight a secret plan"
+  else:
+    row.plan = eval(row['plan'])
+    option1 = row['plan'][0]
+    option2 = row['plan'][1]
+    option3 = row['plan'][2]
+    option4 = row['plan'][3]
+    inst = "Read the spans of text that highlight a conspiracy plan"
 
-    </section>
 
-  </body>
-</html>
-'''
+  s = f'''
+  <!DOCTYPE html>
+  <html lang="en-us">
+    <head>
+      <meta charset="UTF-8">
+      <title>Cayman</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="theme-color" content="#157878">
+      <link rel="stylesheet" href="../css/normalize.css">
+      <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+      <link rel="stylesheet" href="../css/cayman.css">
+    </head>
+    <body>
+      <section class="page-header">
+      </section>
 
-with open('posts/2.html', 'w', encoding='utf-8') as f:
-    f.write(s)
+      <section class="main-content">
+  <h2>Read the Telegram Message</h2>
+  <p>{post}</p>
+
+  <h2>{inst}</h2>
+  <ul>
+      <li><b>OPTION 1</b> {option1}</li>
+      <li><b>OPTION 2</b> {option2}</li>
+      <li><b>OPTION 3</b> {option3}</li>
+      <li><b>OPTION 4</b> {option4}</li>
+  </ul>
+
+      </section>
+
+    </body>
+  </html>
+  '''
+
+  with open(f'posts/{row.page_id}.html', 'w', encoding='utf-8') as f:
+      f.write(s)
